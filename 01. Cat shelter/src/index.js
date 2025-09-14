51e32db4a3dd5b22f2ea27736fa1d568586e918d
Daniel Bratov <daniel.bratov@unicontsoft.com>
2025-09-14 12:02:21 +0300

    Implement web sever

--- 01. Cat shelter/src/index.js
+++ 01. Cat shelter/src/index.js
@@ -1,1 +1,10 @@
-console.log(111);
+import http from 'http';
+
+const server = http.createServer((req, resp) => {
+    resp.write("test");
+
+    resp.end();
+});
+
+server.listen(5000);
+console.log("Server is listening on port 5000 - http://localhost:5000");
