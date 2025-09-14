5bb32af6689236755f4a932cfb44603732fb0ead
Daniel Bratov <daniel.bratov@unicontsoft.com>
2025-09-14 13:43:29 +0300

    Implement home page load

--- 01. Cat shelter/src/index.js
+++ 01. Cat shelter/src/index.js
@@ -1,10 +1,21 @@
 import http from 'http';
+import fs from "fs/promises"
 
-const server = http.createServer((req, resp) => {
-    resp.write("test");
+const server = http.createServer(async (req, resp) => {
+    if (req.method === "GET"){        
+        let content = "";
+        if (req.url === "/"){
+            content = await fs.readFile("./src/views/index.html", { encoding: "utf-8" });
+        }
+        else if (req.url === "/styles/site.css"){
+            content = await fs.readFile("./src/styles/site.css", { encoding: "utf-8" }); 
+        }
 
-    resp.end();
+        resp.write(content);
+        resp.end();
+    }
 });
 
-server.listen(5000);
-console.log("Server is listening on port 5000 - http://localhost:5000");
+server.listen(5000, () => {
+    console.log("Server is listening on port 5000 - http://localhost:5000");
+});
